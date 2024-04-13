4acfd319cdfe3f26f4aae7596b832996bf2c4041
arxivj <xivpark1110@gmail.com>
2024-04-13 23:29:45 +0900

    Feat: Flutter_gen 패키지로 fonts, assets 관리

---
+++ lib/presenter/assets.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/icons_dark.riv
+  String get iconsDark => 'assets/icons/icons_dark.riv';
+
+  /// File path: assets/icons/icons_light.riv
+  String get iconsLight => 'assets/icons/icons_light.riv';
+
+  /// File path: assets/icons/theme_switch.riv
+  String get themeSwitch => 'assets/icons/theme_switch.riv';
+
+  /// List of all assets
+  List<String> get values => [iconsDark, iconsLight, themeSwitch];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
